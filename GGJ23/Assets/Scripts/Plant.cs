1177aa5c7f6a996c4ee3d4ea600b280b88757463
Preston Grau <preston.grau@gmail.com>
2023-02-04 00:23:49 -0600

    Plant timer

--- GGJ23/Assets/Scripts/Plant.cs
+++ GGJ23/Assets/Scripts/Plant.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class Plant : MonoBehaviour
 {
+    public enum Type
+    {
+        potato,
+        carrot,
+        parsnips
+    }
+    public Type type = Type.potato;
+    public float timeToGrow = 30f;
+    public Sprite[] growCycles;
+    public bool needsWater = true;
+    public bool isFertilized = false;
+    public bool canHarvest = false;
+    public int price = 30;
+
+    [SerializeField]
+    private Sprite currSprite;
+    [SerializeField]
+    private int currCycle = 0;
+    [SerializeField]
+    private float timeTillHarvest;
+    private SpriteRenderer sprite;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeTillHarvest = timeToGrow;
+        currSprite = growCycles[currCycle];
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.sprite = currSprite;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeTillHarvest > 0)
+        {
+            timeTillHarvest -= Time.deltaTime;
+            int timeChange = (int) (timeToGrow / timeTillHarvest);
+        }
+        else
+        {
+            canHarvest = true;
+        }
+
+        for (int x = growCycles.Length; x > 0; x--)
+        {
+            if (timeTillHarvest <= ((x * timeToGrow) / growCycles.Length))
+            {
+                if (currCycle < growCycles.Length - x)
+                {
+                    currSprite = growCycles[growCycles.Length - x];
+                    sprite.sprite = currSprite;
+                    currCycle = growCycles.Length - x;
+                }
+            }
+        }
     }
 }
